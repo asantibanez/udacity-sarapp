8c8b56560bde50a76a137e37e0d1c8c7d45b4fe4
asantibanez <santibanez.andres@gmail.com>
2016-02-24 16:44:06 -0500

    Added invoice information to view

--- app/src/main/java/com/andressantibanez/sarapp/navigation/invoiceview/InvoiceViewActivity.java
+++ app/src/main/java/com/andressantibanez/sarapp/navigation/invoiceview/InvoiceViewActivity.java
@@ -2,12 +2,20 @@ package com.andressantibanez.sarapp.navigation.invoiceview;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.andressantibanez.sarapp.R;
+import com.andressantibanez.sarapp.database.invoices.Invoice;
+import com.andressantibanez.sarapp.database.invoices.InvoicesCursor;
+import com.andressantibanez.sarapp.database.invoices.InvoicesSelection;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class InvoiceViewActivity extends AppCompatActivity {
@@ -16,6 +24,10 @@ public class InvoiceViewActivity extends AppCompatActivity {
     public static final String INVOICE_ID = "invoice_id";
 
     String mInvoiceId;
+
+    @Bind(R.id.supplier_name) TextView mSupplierNameView;
+    @Bind(R.id.issuing_date) TextView mIssuingDateView;
+    @Bind(R.id.subtotal) TextView mSubtotalView;
 
     public static Intent launchIntent(Context context, String invoiceId) {
         Intent intent = new Intent(context, InvoiceViewActivity.class);
@@ -30,10 +42,37 @@ public class InvoiceViewActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         getParameters();
+
+        //Get invoice data
+        getSupportLoaderManager().initLoader(1000, null, new LoaderManager.LoaderCallbacks<Cursor>() {
+            @Override
+            public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+                return new InvoicesSelection().whereIdEquals(mInvoiceId).getAsCursorLoader();
+            }
+
+            @Override
+            public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+                Invoice invoice = new InvoicesCursor(data).first();
+                displayInvoiceInfo(invoice);
+            }
+
+            @Override
+            public void onLoaderReset(Loader<Cursor> loader) {
+
+            }
+        });
+
+        //Get invoice details data
     }
 
     public void getParameters() {
         mInvoiceId = getIntent().getStringExtra(INVOICE_ID);
         Log.d(LOG_TAG, "Id: " + mInvoiceId);
+    }
+
+    public void displayInvoiceInfo(Invoice invoice) {
+        mSupplierNameView.setText(invoice.supplierName);
+        mIssuingDateView.setText(invoice.readableIssuingDate());
+        mSubtotalView.setText(invoice.readableSubtotal());
     }
 }
